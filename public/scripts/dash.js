b7037d6399dc194b05988883317b6f8f69be11d0
Rajiste Bennin <rajisteb@gmail.com>
2017-08-03 18:36:49 -0400

    API loop working

--- public/scripts/dash.js
+++ public/scripts/dash.js
@@ -5,21 +5,43 @@ $(function() {
 
 
 $(document).ready(function() {
-    $.ajax({
+    let coinList = [];
+    function update() {
+        $.ajax({
         url: 'https://min-api.cryptocompare.com/data/pricemultifull?fsyms=BTC,BCH,ETH,LTC,XEM,XRP,DASH,ZEC,DGB&tsyms=USD',
         success: function(data) {
                 console.log(data);
-                getCoinData(data);
+                showData(data);
         }
-    })
-    function getCoinData(data) {
-        console.log('getting prices...');
-        for (coin in data.DISPLAY) {
-            console.log(coin);
-        }
+    });
+}
+setInterval(update, 5000);
 
-    }
+function showData(data){
+    console.log(data)
+    Object.keys(data.DISPLAY).map((name, i) => {
+        const value = data.DISPLAY[name];
+        $('#'+name).text(value.USD.PRICE);
+    })
+    
+    // $('#bitcoin').text(data.DISPLAY[coin].USD);
+}
+
+    // function showData(coinList) {
+    //     console.log(coinList[0].PRICE);
+    //     $('#bitcoin').text(coinList[0].PRICE);
+    // };
+    // function getCoinData(data) {
+    //     console.log('getting prices...');  
+    //     for (coin in data.DISPLAY) {
+    //         coinList.push(data.DISPLAY[coin].USD);
+    //     }
+    //     console.log(coinList);
+    //     showData(coinList);
+    // }
 });
+
+
 
 
 
