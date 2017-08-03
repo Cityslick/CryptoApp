0e971002cca87b7c0b9eafe9c7d53f09baedeb38
Rajiste Bennin <rajisteb@gmail.com>
2017-08-03 15:14:37 -0400

    iterating thru object

--- public/scripts/dash.js
+++ public/scripts/dash.js
@@ -5,23 +5,23 @@ $(function() {
 
 
 $(document).ready(function() {
+    let bitcoinData;
     console.log('getting prices...');
     $.ajax({
         url: 'https://min-api.cryptocompare.com/data/pricemultifull?fsyms=BTC,BCH,ETH,LTC,XEM,XRP,DASH,ZEC,DGB&tsyms=USD',
         success: function(data) {
                 console.log(data);
-                getCoinData(data);
+                bitcoinData = data;
         }
     })
     function getCoinData(data) {
         for (price in data.DISPLAY) {
-            if (price === currency[0]) {
-                console.log(data.DISPLAY.currency[0].USD);
-            }
+            console.log(price);
         }
 
     }
 });
+
 
 
 
