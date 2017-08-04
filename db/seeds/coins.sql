dfa526993058d1b65c51d50e6533f195b74c2f3a
Rajiste Bennin <rajisteb@gmail.com>
2017-08-04 10:03:06 -0400

    populated database

---
+++ db/seeds/coins.sql
@@ -0,0 +1,48 @@
+INSERT INTO coins (name, symbol, description) VALUES
+(
+    'Bitcoin',
+    'BTC',
+    'Bitcoin uses peer-to-peer technology to operate with no central authority or banks; managing transactions and the issuing of bitcoins is carried out collectively by the network. Although other cryptocurrencies have come before, Bitcoin is the first decentralized cryptocurrency - Its reputation has spawned copies and evolution in the space.
+With the largest variety of markets and the biggest value - having reached a peak of 18 billion USD - Bitcoin is here to stay. As with any new invention, there can be improvements or flaws in the initial model however the community and a team of dedicated developers are pushing to overcome any obstacle they come across. It is also the most traded cryptocurrency and one of the main entry points for all the other cryptocurrencies. The price is as unstable as always and it can go up or down by 10%-20% in a single day.'
+),
+(
+    'Ethereum',
+    'ETH',
+    'Ethereum is a decentralized platform that runs smart contracts: applications that run exactly as programmed without any possibility of downtime, censorship, fraud or third party interference. In the Ethereum protocol and blockchain there is a price for each operation. The general idea is, in order to have things transferred or executed by the network, you have to consume or burn Gas. The cryptocurrency is called Ether and is used to pay for computation time and for transaction fees.
+Ethereum is how the Internet was supposed to work. As long as you have enough funds to pay for your code to be run by the network, your contacts will always be up and running.It was crowdfunded during August 2014 by fans all around the world. It is developed and maintained by ETHDEV with contributions from great minds across the globe. There is also an Ethereum foundation and there are multiple startups working with the Ethereum blockchain.'
+),
+(
+    'Litecoin',
+    'LTC',
+    'Litecoin LTC - provides faster transaction confirmations ,2.5 minutes on average, and uses a memory-hard, scrypt-based mining proof-of-work algorithm to target the regular computers and GPUs most people already have - which are its main differentials to Bitcoin. The Litecoin network is scheduled to produce 84 million currency units with a halving in reward every four years just like bitcoin. The coin was created by a Google employee, Charles Lee. Litecoin is in second spot to Bitcoin and has spawned numerous clones - however it has a solid base of support and dedicated development team. The Litecoin project is currently maintained by a core group of 6 software developers, led by Charles Lee, with a large community that is growing in support.'
+),
+(
+    'Digital Cash',
+    'DASH',
+    'DigitalCash aka DASH uses a new chained hashing algorithm approach, with many new scientific hashing algorithms for the proof-of-work. DASH aims to be the first privacy-centric cryptographic currency with fully encrypted transactions and anonymous block transactions, this feature is called PrivateSend and can be found on the official Dashcore wallet. PrivateSend mixes your DASH coins with other users who are also using this feature at the time, making your transactions untraceable. Users can also earn DigitalCash rewards by hosting a master node to help maintain the Blockchain. One must have a balance of at least 1000 DASH in order to host a Dash master node, this collateral is required to avoid 51% attacks on the network.  '
+),
+(
+    'Ripple',
+    'XRP',
+    'Ripple positions itself as a complement to, rather than a competitor with, Bitcoin - the site has a page dedicated to Ripple for bitcoiners. Ripple is a distributed network which means transactions occur immediately across the network - and as it is peer to peer - the network is resilient to systemic risk. Ripples are not mined unlike bitcoin and its peers - but each transaction destroys a small amount of XRP which adds a deflationary measure into the system. There are 100 billion XRP at present.'
+),
+(
+    'ZCash',
+    'ZEC',
+    'ZCash is a privacy driven cryptocurrency. It uses the Equihash as an algorithm, which is an asymmetric memory-hard Proof of Work algorithm based on the generalized birthday problem. It relies on high RAM requirements to bottleneck the generation of proofs and making ASIC development unfeasible. ZCash uses  zero-knowledge Succinct Non-interactive Arguments of Knowledge to ensure that all information like sender, receiver, amount is encrypted, without the possibility of double-spending. The only information that is revealed regarding transactions is the time in which they take place.'
+),
+(
+    'NEM',
+    'XEM',
+    'NEM is a peer-to-peer crypto platform. It is written in Java and JavaScript with 100% original source code. NEM has a stated goal of a wide distribution model and has introduced new features in blockchain technology in its proof-of-importance algorithm. NEM also features an integrated P2P secure and encrypted messaging system, multisignature accounts and an Eigentrust++ reputation system. NEM has gone through extensive open alpha testing starting June 25, 2014, followed by lengthy and comprehensive beta testing starting on October 20, 2014. NEM finally launched on May 31, 2015.'
+),
+(
+    'BitCash',
+    'BCH',
+    'Bitcoin Cash is a hard forked version of the original Bitcoin. It is similar to bitcoin with regards to its protocol; Proof of Work SHA-256 hashing, 21,000,000 supply, same block times and reward system. However two main differences are the the blocksize limits, as of August 2017 Bitcoin has a 1MB blocksize limit whereas BCH proposes 8MB blocks. Also BCH will adjust the difficulty every 6 blocks as opposed to 2016 blocks as with Bitcoin. Bitcoin Cash is a proposal from the viaBTC mining pool and the Bitmain mining group to carry out a User Activated Hard Fork on August 1st 12:20 pm UTC. They rejected the agreed consensus aka BIP-91 or SegWit2x and have decided to fork the original Bitcoin blockchain and create this new version called “Bitcoin Cash”. Bitcoin Cash can be claimed by BTC owners who have their private keys or store their Bitcoins on a service that will split BCH for the customer.'
+),
+(
+    'Digibyte',
+    'DGB',
+    'Digibyte is a coin that has adopted the Bitcoin figure but increased the supply to 21 billion in order to keep mining local and in the hands of many. A 60 second block target, a 0.5% premine and a block retarget every 2.4 hrs or 244 blocks. 21 billion DigiBytes will be created over 21 years. Think of a block as an excel spreadsheet that lists the address location of all DigiBytes at a given point in time in history. Mining is how transactions are processed on the network. Anyone can become a miner by donating and using their desktop, laptop or mobile phone computing power to help process transactions on the DigiByte network. The DigiByte blockchain is the entire history of all blocks discovered on the network & therefore all transactions made on the network. By linking blocks together an accurate, secure accounting of all up to date DigiByte ownership is made by decentralized consensus.'
+)
--- db/seeds/seed.sql
+++ db/seeds/seed.sql
@@ -1,3 +1,3 @@
 \c cryptocurrency
 
-
+\i coins.sql
